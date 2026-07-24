639b30ca892a492312d3e41f5fee7495ce8f7d82
Dayana <dayanaa1819-crypto@users.noreply.github.com>
2026-07-24 15:08:29 -0500

    feat: app con express

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (_, res) => {
+    res.send("Hola mundo");
+});
+
+app.listen(port, () => {
+    console.log(`Servidor en funcionamiento en el puerto: ${port}`);
+});
