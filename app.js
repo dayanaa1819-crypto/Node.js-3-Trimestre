5082984ca3552761fd21272d17d79ae2f7835cfc
adrian palacios <adrianpalacios.com>
2026-07-31 17:13:12 -0500

    Ejercicios

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
 import express from 'express';
-import {configDotenv} from 'dotenv';
-configDotenv();
+import "dotenv/config"
 
 const app = express();
 
-const port = process.env.PUERT || 3000;
+const port = process.env.PUERTO|| 3000;
 
 app.get("/", (_, res) => {
-    res.send("Hola mundo");
+    res.send("Aprendices Sena 3407186");
 });
+app.get("/ruta1", (req, res) => {
+    res.send('<h1>Usando res.send</h1>')
+})
+app.get("/ruta2", (req, res) => {
+    res.json({"dev":"node --watch app.js", "script":"node app.js"})
+})
+app.get("/ruta3/:nombre/:apellido", (req, res) => {
+    const nameUsuario = req.params.nombre
+    const apellido =req.params.apellido
+    res.json({"usuario": nameUsuario,"apellido": apellido})
+})    
 
-app.listen(port, () => {
-    console.log(`SERVIDOR: http://localhost:${port}`);
+app.get("/ruta4", (req, res) => {
+    const numero = req.query.phone || 3209423122
+    const orden = req.query.orden || "sin orden"
+    const pagina = req.query.pagina || 1
+    res.send(`<h1>Listado Aprendices</h1>
+        <h2>El listado en orden: ${orden}</h2>
+        <p>Pagina ${pagina}</p>
+        <h3>Numero: ${numero}</h3>`)
+})
+
+app.get("/ruta5/:saludo/:nombre", (req, res) => {
+    const saludo = req.params.saludo;
+    const nombre = req.params.nombre;
+
+    if (nombre.length < 3) {
+        return res.status(400).send("Error: El nombre debe tener al menos 3 caracteres");
+    }
+
+    res.send(`<h1>${saludo},${nombre}</h1>`);
+}); 
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`SERVIDOR: http://localhost:${PORT}`);
 });
